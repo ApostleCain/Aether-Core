682d63642cf20a3c5552ce90a8392f7c714038f0
ApostleCain <andrewxfrosty@gmail.com>
2026-02-02 15:55:44 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+{
+  "name": "Aether Core",
+  "short_name": "Aether",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#050505",
+  "theme_color": "#00f2ff",
+  "icons": [
+    {
+      "src": "emerald_icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
